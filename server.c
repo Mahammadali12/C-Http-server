67e72c947f51c95176b99bf79619f11a0574f2f5
Mahammadali12 <zamanli.mehemmedeli@gmail.com>
2025-01-20 12:13:14 -0500

    Somehow working Client server

--- server.c
+++ server.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <string.h>
 
 #define PORT "8080"
 #define BACKLOG 10
@@ -14,10 +15,21 @@ int main(void)
     struct addrinfo* res;
     int socket_fd;
     
-    
-    getaddrinfo(NULL,"80",&hints,&res);
+    memset(&hints,0,sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
 
-    socket_fd = socket(res->ai_family,res->ai_socktype,res->ai_protocol);
+    
+    if( getaddrinfo(NULL,PORT,&hints,&res) == -1)
+    {
+        perror("ERROR GETADDRINF()");
+    }
+
+    if((socket_fd = socket(res->ai_family,res->ai_socktype,res->ai_protocol)) == -1)
+    {
+        perror("ERROR CREATIN SOCKET");
+    }
 
     if(bind(socket_fd,res->ai_addr,res->ai_addrlen) == -1)
     {
@@ -32,12 +44,14 @@ int main(void)
     
 
     int client_fd;
-    if((client_fd = accept(socket_fd,res->ai_addr,res->ai_addrlen)) == -1)
+    struct sockaddr_storage client_addr;
+    socklen_t addr_size;
+    if((client_fd = accept(socket_fd,(struct sockaddr*)&client_addr,&addr_size)) == -1)
     {
         perror ("ERROR accepting");
     }
 
     printf("SUCCESS client: %d  server:%d\n",client_fd,socket_fd);
 
-    
+    freeaddrinfo(res);
 }
